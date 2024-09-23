aa620bcd3f98da62f6068620d849658f360affd7
JustLuketh <148896722+JustLuketh@users.noreply.github.com>
2024-09-23 12:32:43 +0100

    Gouging Fire Pokedex entry

---
+++ data/pokemon/dex_entries/gougingfire.asm
@@ -0,0 +1,10 @@
+	db "PARADOX@" ; species name
+	dw 1106, 13007 ; height, weight
+
+	db   "This brawny"
+	next "#MON origins"
+	next "is unknown.,"
+
+	page "Its said to be"
+	next "a past version"
+	next "of ENTEI.@"
