b6b7638d9c3a49ad28e70bdb09775ea07ce9aca9
JustLuketh <148896722+JustLuketh@users.noreply.github.com>
2024-09-23 11:51:21 +0100

    Walking Wake Pokedex entry

---
+++ data/pokemon/dex_entries/walkingwake.asm
@@ -0,0 +1,10 @@
+	db "PARADOX@" ; species name
+	dw 1106, 6173 ; height, weight
+
+	db   "This divine"
+	next "#MON came"
+	next "from a past land,"
+
+	page "Its said to"
+	next "be similar to"
+	next "SUICUNE.@"
