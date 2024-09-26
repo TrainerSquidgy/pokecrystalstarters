46273602ec0d960b749a14c18fc20a62637ea403
TrainerSquidgy <144160413+TrainerSquidgy@users.noreply.github.com>
2024-09-26 22:25:14 +0100

    Weather Tweaks
    - Consolidated all the Weather commands into one routine to save space.
    
    - Added in SNOW and HAIL to the base code, inaccessible without adding moves, but functional with just the need to add the move in.

--- data/text/battle.asm
+++ data/text/battle.asm
@@ -1106,3 +1106,36 @@ BattleText_NoTimeLeftToday: ; unreferenced
 	text "There is no time"
 	line "left today!"
 	done
+
+BattleText_HailContinuesToFall:
+	text "Hail continues to"
+	line "fall."
+	prompt
+	
+BattleText_TheHailStopped:
+	text "The hail stopped."
+	prompt
+
+ItStartedToHailText:
+	text "It started"
+	line "to hail!"
+	prompt
+
+PeltedByHailText:
+	text "<USER>"
+	line "is pelted by HAIL!"
+	prompt
+
+BattleText_SnowContinuesToFall:
+	text "Snow continues to"
+	line "fall."
+	prompt
+	
+BattleText_TheSnowStopped:
+	text "The snow stopped."
+	prompt
+
+ItStartedToSnowText:
+	text "It started"
+	line "to snow!"
+	prompt
