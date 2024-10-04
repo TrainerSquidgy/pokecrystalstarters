b379580038075ef8fe765f90032268428416b5a2
TrainerSquidgy <144160413+TrainerSquidgy@users.noreply.github.com>
2024-10-04 16:41:44 +0100

    Fixed Rock Tomb!

--- data/moves/moves.asm
+++ data/moves/moves.asm
@@ -265,7 +265,7 @@ Moves:
 	move ROCK_SMASH,   EFFECT_DEFENSE_DOWN_HIT,   20, FIGHTING,     100, 15,  50
 	move WHIRLPOOL,    EFFECT_TRAP_TARGET,        15, WATER,         70, 15,   0
 	move BEAT_UP,      EFFECT_BEAT_UP,            10, DARK,         100, 10,   0
-	move ROCK_TOMB,    EFFECT_SPEED_DOWN_HIT,     50, ROCK,          85, 10, 100
+	move ROCK_TOMB,    EFFECT_SPEED_DOWN_HIT,     60, ROCK,          95, 10, 100
 	move ROCK_BLAST,   EFFECT_MULTI_HIT,     	  25, ROCK,          80, 10,   0
 	move X_SCISSOR,    EFFECT_NORMAL_HIT,     	  80, BUG,          100, 15,   0
 	assert_table_length NUM_ATTACKS
