7aadb1ca68ff319793b53495bd4f298c5b74cd96
JustLuketh <148896722+JustLuketh@users.noreply.github.com>
2024-10-08 18:33:37 +0100

    Annihilape backport started!

---
+++ data/pokemon/dex_entries/annihilape.asm
@@ -0,0 +1,10 @@
+	db "RAGE MONKEY@" ; species name
+	dw 311, 1235 ; height, weight
+
+	db   "Its anger rose"
+	next "beyond a critical"
+	next "point, it gained"
+
+	page "power that is"
+	next "unbound by its"
+	next "bodys limits.@"
