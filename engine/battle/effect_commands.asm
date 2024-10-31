6877501d0611f0059e553185f8efe21f919f780a
TrainerSquidgy <144160413+TrainerSquidgy@users.noreply.github.com>
2024-10-31 19:11:42 +0000

    Spiritomb Updated

--- engine/battle/effect_commands.asm
+++ engine/battle/effect_commands.asm
@@ -6929,30 +6929,6 @@ GetNextTypeMatchupsByte:
 
 
 BattleCommand_AddDamage:
-	push af
-	push hl
-    ld hl, wCurDamage + 1
-    ld a, [hl]           
-    ld d, a              
-    dec hl               
-    ld a, [hl]           
-    ld e, a              
-    ld hl, wCurDamage    
-    ld a, [hl]           
-    add a, e             
-    ld [hl], a           
-    inc hl               
-    ld a, [hl]           
-    adc a, d             
-    ld [hl], a           
-    jr nc, .done         
-    ld a, $FF            
-    ld hl, wCurDamage    
-    ld [hl], a           
-    ld [hli], a         
-.done:
-	pop hl
-	pop af
     ret
 	
 	
