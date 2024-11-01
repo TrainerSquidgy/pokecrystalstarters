fd88e40504044aa811f4792b9b2087e81eea2da4
TrainerSquidgy <144160413+TrainerSquidgy@users.noreply.github.com>
2024-11-01 02:24:22 +0000

    Consolidation

--- data/moves/effects.asm
+++ data/moves/effects.asm
@@ -2093,23 +2093,3 @@ Snow:
 	startweather
 	endmove
 	
-PLAHiddenPower:
-	checkobedience
-	usedmovetext
-	doturn
-	critical
-	plahiddenpower
-	damagestats
-	damagecalc
-	stab
-	damagevariation
-	checkhit
-	moveanim
-	failuretext
-	applydamage
-	criticaltext
-	supereffectivetext
-	checkfaint
-	buildopponentrage
-	kingsrock
-	endmove
