80530c7af2b8b7110a0cd25d2a9557ed5bda8bb3
TrainerSquidgy <144160413+TrainerSquidgy@users.noreply.github.com>
2025-05-06 16:27:44 +0100

    Pokémon Data Files Added

---
+++ data/pokemon/dex_entries/magnezone.asm
@@ -0,0 +1,10 @@
+	db "MAGNET AREA@" ; species name
+	dw 311, 3968 ; height, weight
+
+	db   "Exposure to a"
+	next "special magnetic"
+	next "field has made"
+
+	page "the molecular"
+	next "structure change"
+	next "into MAGNEZONE.@"
