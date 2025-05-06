e5bc7589cfda97d00dacd814def10fba2db60503
TrainerSquidgy <144160413+TrainerSquidgy@users.noreply.github.com>
2025-05-06 18:49:12 +0100

    Metal Sound Fix

--- data/moves/moves.asm
+++ data/moves/moves.asm
@@ -26,7 +26,7 @@ Moves:
 	move SCRATCH,      EFFECT_NORMAL_HIT,         40, NORMAL,       100, 35,   0
 	move VICEGRIP,     EFFECT_NORMAL_HIT,         55, NORMAL,       100, 30,   0
 	move GUILLOTINE,   EFFECT_OHKO,                0, NORMAL,        30,  5,   0
-	move METAL_SOUND,  EFFECT_DEFENSE_DOWN_2,      0, STEEL,         85, 40,   0
+	move METAL_SOUND,  EFFECT_SP_DEF_DOWN_2,       0, STEEL,         85, 40,   0
 	move SWORDS_DANCE, EFFECT_ATTACK_UP_2,         0, NORMAL,       100, 30,   0
 	move CUT,          EFFECT_NORMAL_HIT,         50, NORMAL,        95, 30,   0
 	move GUST,         EFFECT_GUST,               40, FLYING,       100, 35,   0
