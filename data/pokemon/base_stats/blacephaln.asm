be13340cf0e40b74f4ad698770fa23c65d816545
TrainerSquidgy <144160413+TrainerSquidgy@users.noreply.github.com>
2025-05-11 23:02:01 +0100

    Files Added

---
+++ data/pokemon/base_stats/blacephaln.asm
@@ -0,0 +1,21 @@
+	db BLACEPHALN ; 150
+
+	db 106, 110,  90, 130, 154,  90
+	;   hp  atk  def  spd  sat  sdf
+
+	db PSYCHIC_TYPE, PSYCHIC_TYPE ; type
+	db 30 ; catch rate
+	db 255 ; base exp
+	db NO_ITEM, BERSERK_GENE ; items
+	db GENDER_UNKNOWN ; gender ratio
+	db 100 ; unknown 1
+	db 120 ; step cycles to hatch
+	db 5 ; unknown 2
+	INCBIN "gfx/pokemon/blacephaln/front.dimensions"
+	dw NULL, NULL ; unused (beta front/back pics)
+	db GROWTH_SLOW ; growth rate
+	dn EGG_NONE, EGG_NONE ; egg groups
+
+	; tm/hm learnset
+	tmhm HEADBUTT, CURSE, TOXIC, HIDDEN_POWER, SUNNY_DAY, SNORE, HYPER_BEAM, PROTECT, ENDURE, FRUSTRATION, SOLARBEAM, RETURN, PSYCHIC_M, SHADOW_BALL, DOUBLE_TEAM, SWAGGER, SLEEP_TALK, FIRE_BLAST, REST, ATTRACT, THIEF, FIRE_PUNCH, FLAMETHROWER
+	; end
---
+++ data/pokemon/dex_entries/blacephaln.asm
@@ -0,0 +1,10 @@
+	db "FIREWORKS@" ; species name
+	dw 511, 287 ; height, weight
+
+	db   "It slithers towa-"
+	next "rds people. Then,"
+	next "without warning,"
+
+	page "it explodes its"
+	next "own head. It is"
+	next "an ULTRA BEAST.@"
