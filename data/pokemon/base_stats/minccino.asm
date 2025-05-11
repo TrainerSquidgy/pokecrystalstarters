9a7ee62ffdd517dfcda1407216b7f589affedc1f
TrainerSquidgy <144160413+TrainerSquidgy@users.noreply.github.com>
2025-05-11 17:10:00 +0100

    Fixed Exp Group

--- data/pokemon/base_stats/minccino.asm
+++ data/pokemon/base_stats/minccino.asm
@@ -13,7 +13,7 @@
 	db 5 ; unknown 2
 	INCBIN "gfx/pokemon/minccino/front.dimensions"
 	dw NULL, NULL ; unused (beta front/back pics)
-	db GROWTH_MEDIUM_FAST ; growth rate
+	db GROWTH_FAST ; growth rate
 	dn EGG_GROUND, EGG_GROUND ; egg groups
 
 	; tm/hm learnset
