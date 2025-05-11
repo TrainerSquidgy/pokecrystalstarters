cabb19e7b3fe2a2e59826c02bbec7aba0c53cd60
TrainerSquidgy <144160413+TrainerSquidgy@users.noreply.github.com>
2025-05-11 16:43:14 +0100

    File Structure Added

---
+++ data/pokemon/dex_entries/cinccino.asm
@@ -0,0 +1,10 @@
+	db "SCARF@" ; species name
+	dw 108, 165 ; height, weight
+
+	db   "Their white fur"
+	next "feels amazing to"
+	next "touch. It repels"
+
+	page "dust and prevents"
+	next "static from"
+	next "building up.@"
---
+++ data/pokemon/dex_entries/minccino.asm
@@ -0,0 +1,10 @@
+	db "CHINCHILLA@" ; species name
+	dw 104, 128 ; height, weight
+
+	db   "They greet each"
+	next "other by rubbing"
+	next "their tails, that"
+
+	page "are always kept"
+	next "well groomed"
+	next "and clean.@"
