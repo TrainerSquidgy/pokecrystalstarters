e709a346b9ce70c98df17369c68c921a9997806d
TrainerSquidgy <144160413+TrainerSquidgy@users.noreply.github.com>
2025-05-19 18:20:42 +0100

    Files in Place

---
+++ data/pokemon/dex_entries/buizel.asm
@@ -0,0 +1,10 @@
+	db "SEA WEASEL@" ; species name
+	dw 204, 650 ; height, weight
+
+	db   "It spins its two"
+	next "tails like a"
+	next "screw to propel"
+
+	page "itself through"
+	next "the water very"
+	next "gracefully.@"
---
+++ data/pokemon/dex_entries/floatzel.asm
@@ -0,0 +1,10 @@
+	db "SEA WEASEL@" ; species name
+	dw 307, 739 ; height, weight
+
+	db   "It is a common"
+	next "sight around"
+	next "fishing ports."
+
+	page "It will rescue"
+	next "people in danger"
+	next "in the water.@"
