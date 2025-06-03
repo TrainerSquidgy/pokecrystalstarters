8324832317937f84108131e929a42cfc3f8e61eb
TrainerSquidgy <144160413+TrainerSquidgy@users.noreply.github.com>
2025-06-03 09:46:34 +0100

    Files Added

---
+++ data/pokemon/dex_entries/zangoose.asm
@@ -0,0 +1,10 @@
+	db "CAT FERRET@" ; species name
+	dw 403, 888 ; height, weight
+
+	db   "It has a rivalry"
+	next "with SEVIPER. If"
+	next "it sees one its"
+
+	page "fur bristles and"
+	next "it will prepare"
+	next "for battle.@"
