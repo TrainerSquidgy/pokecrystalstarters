2160b556ee614b87ade84985d35703fe9d99964d
TrainerSquidgy <144160413+TrainerSquidgy@users.noreply.github.com>
2025-09-09 06:06:33 +0100

    Files in place

---
+++ data/pokemon/dex_entries/voctaria.asm
@@ -0,0 +1,10 @@
+	db "OPERATIC@" ; species name
+	dw 507, 1800 ; height, weight
+
+	db   "It believes it"
+	next "is a skilled"
+	next "opera singer."
+
+	page "However, her"
+	next "songs send people"
+	next "to sleep.@"
