bc9f1006b0699ad0da59987f71b85597c95b9dfc
TrainerSquidgy <144160413+TrainerSquidgy@users.noreply.github.com>
2025-09-12 10:25:15 +0100

    Files in place

---
+++ data/pokemon/dex_entries/dudunsparce.asm
@@ -0,0 +1,10 @@
+	db "LAND SNAKE@" ; species name
+	dw 1110, 860 ; height, weight
+
+	db   "It uses its hard"
+	next "tail to nest deep"
+	next "underground."
+
+	page "The nest can"
+	next "reach up to six"
+	next "miles long.@"
