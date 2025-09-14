61c780c160afa039a8b2a543c69ed98a85e179ff
TrainerSquidgy <144160413+TrainerSquidgy@users.noreply.github.com>
2025-09-14 03:32:43 +0100

    Files in Place

---
+++ data/pokemon/dex_entries/weavile.asm
@@ -0,0 +1,10 @@
+	db "SHARP CLAW@" ; species name
+	dw 307, 750 ; height, weight
+
+	db   "Evolution made"
+	next "it even more"
+	next "devious. It"
+
+	page "communicates by"
+	next "clawing signs"
+	next "in boulders.@"
