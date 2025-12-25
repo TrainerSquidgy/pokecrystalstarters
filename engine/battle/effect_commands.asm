53115c1c79a507eadd9d24f5449a729b13b5542f
TrainerSquidgy <144160413+TrainerSquidgy@users.noreply.github.com>
2025-12-25 22:25:49 +0000

    Started Moves

--- engine/battle/effect_commands.asm
+++ engine/battle/effect_commands.asm
@@ -7062,3 +7062,20 @@ BattleCommand_SpitUp:
 	call AnimateFailedMove
 	jp PrintButItFailed
 	
+BattleCommand_Ingrain:
+; Fails if the user is already ingrained
+	ld a, BATTLE_VARS_SUBSTATUS5
+	call GetBattleVarAddr
+	bit SUBSTATUS_INGRAINED, [hl]
+	jr nz, .already_ingrained
+
+	set SUBSTATUS_INGRAINED, [hl]
+	
+	call AnimateCurrentMove
+	ld hl, PlantedRootsText
+	jp StdBattleTextbox
+
+.already_ingrained
+	farcall AnimateFailedMove
+	farcall PrintButItFailed
+	ret
