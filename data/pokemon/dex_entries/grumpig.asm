3af1c5f85461cf0635ee9de1b6de15af14d42b07
TrainerSquidgy <144160413+TrainerSquidgy@users.noreply.github.com>
2025-12-25 16:24:11 +0000

    Files in place

---
+++ data/pokemon/dex_entries/grumpig.asm
@@ -0,0 +1,10 @@
+	db "MANIPULATE@" ; species name
+	dw 211, 1576 ; height, weight
+
+	db   "It gains control"
+	next "over its foes"
+	next "by doing odd"
+
+	page "dance steps."
+	next "It wears gems"
+	next "on its head.@"
---
+++ data/pokemon/dex_entries/spoink.asm
@@ -0,0 +1,10 @@
+	db "BOUNCE@" ; species name
+	dw 204, 675 ; height, weight
+
+	db   "It carries the"
+	next "pearl of a"
+	next "CLAMPERL on its"
+
+	page "head. Nobody"
+	next "knows how it"
+	next "obtained it.@"
