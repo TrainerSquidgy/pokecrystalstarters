e6e497a2d60412e4f966e4386e6cc7c7c20146d8
TrainerSquidgy <144160413+TrainerSquidgy@users.noreply.github.com>
2025-12-26 02:43:50 +0000

    Files in place

---
+++ data/pokemon/dex_entries/ogerpon.asm
@@ -0,0 +1,10 @@
+	db "MASK@" ; species name
+	dw 311, 877 ; height, weight
+
+	db   "It changes type"
+	next "based on which"
+	next "mask it wears."
+
+	page "It can draw out"
+	next "energy from each"
+	next "of its masks.@"
